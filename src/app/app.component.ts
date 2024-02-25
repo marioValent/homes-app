4434fad5ad5acff8d10d644b1e8b25b1320923ba
Mario Valent <valent_mario97@yahoo.com>
2024-02-26 01:29:37 +0200

    adding the angular built router to the app and use the HomeComponent as the main page using path:''

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
 @Component({
   standalone: true,
   selector: "app-root",
@@ -14,12 +15,12 @@ import { HomeComponent } from "./home/home.component";
         />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
   styleUrls: ["./app.component.css"],
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
   title = "homes";
